d0474780cd90c0e1d486d51b04a5242adbb5c59a
joeltheai <joeltheai@users.noreply.github.com>
2022-08-02 11:35:39 +0530

    hello

--- fun stuff/joel.py
+++ fun stuff/joel.py
@@ -3,3 +3,24 @@ print("hello World")
 j = "jel"
 
 print(j)
+
+
+# Introduction
+
+# Population:- It is the totality of persons ,objects, items or anything
+# conceivable pertaining certain characteristic
+# Sampling:- Selection of a subset of individuals from within a statistical
+# population to estimate characteristics of the whole population
+# Sample:- Finite part of a population or a subset of sampling units selected
+# by some process, usually by deliberate selection with the object of
+# investigating the properties of the parent population or set
+# Sampling Unit:- Each individual and indivisible units in population
+
+# Objectives
+# 1. Select randomly 100 samples of size 35 from weight and find their sample
+# means and display the same in a table and infer the same.
+# 2. Select randomly 25 samples of size 7 from the fish type Perch with length2
+# and find the sample mean sampling distribution for the same.
+
+# 3. Construct the histogram for the sample mean which was generated in step 2
+# and infer the same.
