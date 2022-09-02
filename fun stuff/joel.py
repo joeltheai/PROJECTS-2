9647ad372163f8b68ff52adc1da5c2ff82086cc6
joeltheai <joeltheai@users.noreply.github.com>
2022-09-03 00:43:03 +0530

    mm

--- fun stuff/joel.py
+++ fun stuff/joel.py
@@ -1,31 +1,41 @@
-print("hello World")
-
-j = "jel"
-
-print(j)
 from typing import List
 
 
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = {}
+
+    def topKFrequent(self, nums: List[int], element: int) -> List[int]:
+        memo = {}
+        l = []
+
 
         for i in nums:
-            if i not in d:
-                d[i] = 1
+            if i not in memo:
+                memo[i] = 1
             else:
-                d[i] = d[i] + 1
-        l = []
-        while k>0:
-            maxi = 0
+                memo[i] = memo[i] + 1
+ 
+
+
+
+
+       
+        while element>0:
+            maximum = 0
             p = 0
-            for i in d:
-                if d[i] > maxi:
-                    maxi = i
+            for i in memo:
+                if memo[i] > maximum:
+                    maximum = i
                     p = i
-            l.append(maxi)
-            del d[p]
-            k = k-1
+            l.append(maximum)
+            del memo[p]
+            element = element-1
         return l
 
 print(Solution.topKFrequent(1,[1,1,1,2,2,3],2))
+
+print("hello world")
+print("i love ubuntu")
+
+
+
